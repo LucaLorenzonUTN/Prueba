aa2000bfa20e9199c5cb198903608487ac2efe14
tomasjuliano <tomasjuliano00@gmail.com>
2024-08-23 18:10:45 -0300

    n

--- piton.py
+++ piton.py
@@ -1,10 +1,6 @@
 print ("Hola")
-<<<<<<< HEAD
 print ("le rompo la cabeza a tomas")
-=======
-
 i=0
-
 while(i <=2):
     if i==0: print("Aguante")
     i=i+1
@@ -14,4 +10,6 @@ while(i <=2):
     
     if i==2: print("millo")
     i=i+1
->>>>>>> 63f9c1ca6c6b526b93c466347c64533c778ade8a
+
+
+    hola luca
